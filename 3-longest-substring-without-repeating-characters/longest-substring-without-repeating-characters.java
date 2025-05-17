cc61d678b0eea0a5f1cecacc36983da1bab25b9e
Ayush Patil <90106829+ayushpatil9696@users.noreply.github.com>
2025-05-17 23:13:17 +0530

    Time: 7 ms (36.41%) | Memory: 45.2 MB (15.35%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -3,15 +3,19 @@ class Solution {
         int left = 0;
         int maxLength = 0;
         HashSet<Character>charSet = new HashSet<>();
+        // using hashset, it store unique value unlike the hashMap 
+
 
         for (int right = 0; right < s.length(); right++){
-            while (charSet.contains(s.charAt(right))) {
-                charSet.remove(s.charAt(left));
-                left++;
+            //ittirating through the s string 
+            while (charSet.contains(s.charAt(right))) { // while hashset contains the character of the right pointer
+                charSet.remove(s.charAt(left)); // we remove the character at left pointer 
+                left++; // and move the left pointer
             }
 
-            charSet.add(s.charAt(right));
-            maxLength = Math.max(maxLength, right - left + 1);
+            charSet.add(s.charAt(right)); // then add the right pointer character in the hashset
+            maxLength = Math.max(maxLength, right - left + 1); // right p - left p to find the length and +1 
+            // since our s array starts from 0 
         }
         
         return maxLength;
